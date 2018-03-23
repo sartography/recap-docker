7d09a0f84267887f53301ec7700713733bc1baa0
kirankumar03 <akula.kirankumar@htcindia.com>
2018-03-23 20:36:13 +0530

    MS 216 NI should be added as a Delivery and PWD to the customer code NS.

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -4,4 +4,8 @@ USE `recap` ;
 -- MS-223 starts here
 UPDATE `recap`.`customer_code_t` SET `DELIVERY_RESTRICTIONS`='BT,BZ,CS,MP,MZ,RS,RH',`PWD_DELIVERY_RESTRICTIONS`='BT,BZ,CS,MP,MZ,RS,RH' WHERE `CUSTOMER_CODE`='UA';
 -- MS-223 ends here
+-- MS-216 starts here
+UPDATE `recap`.`customer_code_t` SET `DELIVERY_RESTRICTIONS`='NS,NC,NI,NK,NX,IN,SA,SM,SP,NY,SR,OM', `PWD_DELIVERY_RESTRICTIONS`='NS,NC,NI,NK,NX,SA,SM,SP,NY,SR' WHERE `CUSTOMER_CODE`='NS';
+-- MS-216 ends here
 -- Script for Release 1.2.5 ends here
+
