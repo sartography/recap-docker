067a447ae1958392a1c4f8d458eee8c12974c0e6
harikrishnanv <harikrishnan.vijaymohan@htcindia.com>
2018-06-01 19:29:37 +0530

    MS-228 PUL delivery location changes

--- scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
+++ scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
@@ -1,5 +1,15 @@
 USE `recap`
 
+-- Script for Release 1.2.5 starts here
+-- MS-223 starts here
+UPDATE `recap`.`customer_code_t` SET `DELIVERY_RESTRICTIONS`='BT,BZ,CS,MP,MZ,RS,RH',`PWD_DELIVERY_RESTRICTIONS`='BT,BZ,CS,MP,MZ,RS,RH' WHERE `CUSTOMER_CODE`='UA';
+-- MS-223 ends here
+-- MS-216 starts here
+UPDATE `recap`.`customer_code_t` SET `DELIVERY_RESTRICTIONS`='NS,NC,NI,NK,NX,IN,SA,SM,SP,NY,SR,OM', `PWD_DELIVERY_RESTRICTIONS`='NS,NC,NI,NK,NX,SA,SM,SP,NY,SR' WHERE `CUSTOMER_CODE`='NS';
+-- MS-216 ends here
+-- Script for Release 1.2.5 ends here
+
+
 -- Script for Release 1.2.4 starts here
 CREATE TABLE IF NOT EXISTS `DEACCESSION_ITEM_CHANGE_LOG_T` (
   `CHANGE_LOG_ID` 	INT 		        NOT NULL AUTO_INCREMENT,
