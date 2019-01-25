d7630ff5cf4f796224e73cea27b3d72067962c88
Hari-Krishnan19 <harikrishnan.vijaymohan@htcindia.com>
2019-01-25 17:42:52 +0530

    MS-286,287,288 Added new status in Request_item_status_t and added a new column in Request_item_t.

--- scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
+++ scsb-mysql/RECAP_ALTER_SCRIPTS_HISTORY.sql
@@ -1,5 +1,21 @@
 USE `recap`
 
+-- Script for Release 1.2.8 Starts here
+
+-- MS-228 Description changes - starts here
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='CR';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='CU';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='EV';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='GC';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='NA';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='GP';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='Firestone Library' WHERE `CUSTOMER_CODE`='PA';
+UPDATE `recap`.`customer_code_t` SET `DESCRIPTION`='' WHERE `CUSTOMER_CODE`='QK';
+
+-- MS-228 Description changes ends here
+
+-- Script for Release 1.2.8 Ends here
+
 -- Script for Release 1.2.7 Starts here
 
 -- MS-228 Starts here
