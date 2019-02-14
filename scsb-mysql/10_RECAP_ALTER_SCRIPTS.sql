d75a31bfafda99c00adafdd8f7f6ffe809dc647f
Hari Krishnan V <harikrishnan.vijaymohan@htcindia.com>
2019-02-14 16:16:32 +0530

    Updated the correct description in Recap alter scripts

--- scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
+++ scsb-mysql/10_RECAP_ALTER_SCRIPTS.sql
@@ -3,6 +3,6 @@ USE `recap` ;
 -- Script for Release 1.2.9 start here
 
 alter table `recap`.`request_item_t` drop column `is_gfa_status_sch`;
-UPDATE `recap`.`request_item_status_t` SET `REQUEST_STATUS_CODE`='LAS_REFILE_REQUEST_PLACED', `REQUEST_STATUS_DESC`='LAS_REFILE_REQUEST_PLACED' WHERE `REQUEST_STATUS_CODE`='REQUESTS_PLACED_ON_SCH';
+UPDATE `recap`.`request_item_status_t` SET `REQUEST_STATUS_CODE`='LAS_REFILE_REQUEST_PLACED', `REQUEST_STATUS_DESC`='LAS REFILE REQUEST PLACED' WHERE `REQUEST_STATUS_CODE`='REQUESTS_PLACED_ON_SCH';
 
 -- Script for Release 1.2.9 ends
